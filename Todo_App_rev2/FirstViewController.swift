6b90faf1c54d757f16c31bf932b532ac7f4f8989
moquito64 <blanchette.sebastian@gmail.com>
2017-12-03 21:27:20 -1000

    this is lit

--- Todo_App_rev2/FirstViewController.swift
+++ Todo_App_rev2/FirstViewController.swift
@@ -7,47 +7,25 @@
 //
 
 import UIKit
-var list = ["Get food", "Do Homework", "Pick up Daryl", "Get new keyboard" ]
-class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    
-    @IBOutlet weak var myTableView: UITableView!
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection : Int) -> Int
-    {
-        return (list.count)
-    }
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default,reuseIdentifier: "cell")
-        cell.textLabel?.text = list[indexPath.row]
+class FirstViewController: UIViewController,UITableViewDelegate{
+   
+    weak var StatusField: UITextView!{
         
-        return(cell)
-    }
-    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
-    {
-        if editingStyle == UITableViewCellEditingStyle.delete
-        {
-            list.remove(at: indexPath.row)
-            myTableView.reloadData()
-        }
-    }
- 
-    override func viewDidAppear(_ animated: Bool) {
-        myTableView.reloadData()
+        self.StatusField!.layer.borderWidth = 1
+        self.StatusField!.layer.borderColor = UIColor.black.cgColor
         
+        return self.StatusField
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
